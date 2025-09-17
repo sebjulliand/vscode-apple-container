f7743420b59468a1ed9cd344ee62a52157f07ec9
Seb Julliand <sjulliand@arcadsoftware.com>
2025-09-17 15:06:46 +0200

    Added output channel
    Signed-off-by: Seb Julliand <sjulliand@arcadsoftware.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,33 @@
 import * as vscode from 'vscode';
 
 let extensionId = "";
-export function activate(context: vscode.ExtensionContext) {	
-	extensionId = context.extension.id;
-	console.log(vscode.l10n.t(`{0} version {1} activated`, extensionId, context.extension.packageJSON.version));
 
+let output: vscode.OutputChannel;
+
+export function activate(context: vscode.ExtensionContext) {
+	extensionId = context.extension.id;
 	context.subscriptions.push(
-		
+		(output = vscode.window.createOutputChannel("Apple Container", "log"))
 	);
+
+	console.log(vscode.l10n.t(`{0} version {1} activated`, extensionId, context.extension.packageJSON.version));
 }
 
 export function deactivate() {
 	console.log(vscode.l10n.t(`{0} deactivated`, extensionId));
+}
+
+export namespace Output {
+	export function appendLineAndThrow(message: string): never {
+		appendLine(vscode.l10n.t("[Error] {0}", message));
+		throw new Error(message);
+	}
+
+	export function append(message: string) {
+		output.append(message);
+	}
+
+	export function appendLine(message: string) {
+		output.appendLine(message);
+	}
 }
